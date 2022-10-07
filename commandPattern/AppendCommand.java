6dad32909eef2d76116b91df47ad1bab4f876114
Jordyn Anderson <jordynanderson@Jordyns-MacBook-Pro-4.local>
2022-10-07 11:22:34 -0400

    append command class

--- commandPattern/AppendCommand.java
+++ commandPattern/AppendCommand.java
@@ -2,5 +2,15 @@ package commandPattern;
 import java.util.Scanner;
 
 public class AppendCommand {
+    private Document doc;
+
+    public AppendCommand(Document doc){
+        this.doc= doc;
+    }
+
+    public String execute(){
+        return execute();
+        
+    }
     
 }
