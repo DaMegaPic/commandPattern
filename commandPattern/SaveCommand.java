784d4b9981bba1241c6fd0feca851448b89e69c6
Jordyn Anderson <jordynanderson@Jordyns-MacBook-Pro-4.local>
2022-10-07 11:23:22 -0400

    save command class

--- commandPattern/SaveCommand.java
+++ commandPattern/SaveCommand.java
@@ -1,5 +1,15 @@
 package commandPattern;
 
 public class SaveCommand {
+    private Document doc;
+
+    public SaveCommand(Document doc){
+        this.doc= doc;
+    }
+
+    public String execute(){
+        return execute();
+        
+    }
     
 }
