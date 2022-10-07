3b9908275308fb9d72430a495e7434b749fceff8
almyrick <myrick.ashley1015@yahoo.com>
2022-10-07 11:18:50 -0400

    view command

--- commandPattern/Command.java
+++ commandPattern/Command.java
@@ -1,5 +1,14 @@
 package commandPattern;
 
 public abstract class Command {
-    
+    protected Document document;
+
+    public Command (Document document){
+
+    }
+
+    public String execute(){
+        return execute();
+        
+    }
 }
--- commandPattern/ViewCommand.java
+++ commandPattern/ViewCommand.java
@@ -2,5 +2,14 @@ package commandPattern;
 public class ViewCommand {
     private Document doc;
 
+    public ViewCommand(Document doc){
+        this.doc= doc;
+    }
+
+    public String execute(){
+        return execute();
+        
+    }
+
     
 }
