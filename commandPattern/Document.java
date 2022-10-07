f6283cec546804566d67eac74ad67958ef789cf5
Colson Carey <damegapic@gmail.com>
2022-10-07 11:18:38 -0400

    Document View Method

--- commandPattern/Document.java
+++ commandPattern/Document.java
@@ -6,4 +6,17 @@ public class Document {
     public String fileName;
     public ArrayList<String>lines = new ArrayList<String>();
     
+    public Document(String fileName){
+        this.fileName = fileName;
+    }
+    public String view(){
+        String docStr = this.lines.get(0);
+        for(int i = 1;i <= this.lines.size();i++){
+            docStr = docStr+this.lines.get(i);
+        }
+        return docStr;
+    }
+    public String append(String line){
+        
+    }
 }
