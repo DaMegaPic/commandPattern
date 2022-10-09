8094cb26ac095257292eb589222332a5cb8c9040
Jordyn Anderson <jordynanderson@Jordyns-MacBook-Pro-4.local>
2022-10-09 18:00:57 -0400

    trying to fix git errors

--- commandPattern/Document.java
+++ commandPattern/Document.java
@@ -1,14 +1,15 @@
 package commandPattern;
-
 import java.util.ArrayList;
 
 public class Document {
+
     public String fileName;
     public ArrayList<String>lines = new ArrayList<String>();
     
     public Document(String fileName){
         this.fileName = fileName;
     }
+
     public String view(){
         String docStr = this.lines.get(0);
         for(int i = 1;i <= this.lines.size();i++){
@@ -16,6 +17,7 @@ public class Document {
         }
         return docStr;
     }
+
     public String append(String line){
         this.lines.add(line);
         return "yes";
@@ -26,4 +28,5 @@ public class Document {
     public String save(){
         
     }
+
 }
